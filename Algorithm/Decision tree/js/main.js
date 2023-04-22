269eeebfb3155752553fff3c8708cacaa6843924
Dmitriy Vasilev <dima.vasiliev2002@mail.ru>
2023-04-22 09:47:08 +0700

    commit

--- Algorithm/Decision tree/js/main.js
+++ Algorithm/Decision tree/js/main.js
@@ -1,30 +1,245 @@
+function entropy(data) {
+    const counts = {};
+    for (let i = 0; i < data.length; i++) {
+        const target = data[i][data[i].length - 1];
+        if (target in counts) {
+            counts[target]++;
+        } else {
+            counts[target] = 1;
+        }
+    }
+    // Вычисляем энтропию
+    let result = 0;
+    for (let key in counts) {
+        const p = counts[key] / data.length;
+        result -= p * Math.log2(p);
+    }
+    return result;
+}
+
+// Функция для вычисления прироста информации
+function informationGain(data, left, right) {
+    const startEntropy = entropy(data);
+    if (startEntropy == 0) { return -1; }
+    const leftEntropy = entropy(left);
+    const rightEntropy = entropy(right);
+    //if (leftEntropy === 0 && rightEntropy === 0) return -1;
+    const leftWeight = left.length / data.length;
+    const rightWeight = right.length / data.length;
+    const result = startEntropy - (leftWeight * leftEntropy) - (rightWeight * rightEntropy);
+    return result;
+}
+
+
+function createTree(container, data, dec = null) {
+    container.append(createTreeDom(data, dec, "", true));
+}
+
+function createTreeDom(obj, dec, dir = "", isChoosen = false) {
+    if (!Object.keys(obj).length) return;
+
+    let ul = document.createElement('ul');
+
+    for (let key in obj) {
+        let li = document.createElement('li');
+
+        li.innerHTML = key;
+
+
+        if (key == "YES" && dir == "Left") {
+            li.innerHTML = `<p style="color:green">${key}</p>`;
+            //li.style = "color:red";
+        }
+        else if (key == "NO" && dir == "Right") {
+            li.innerHTML = `<p style="color:green">${key}</p>`;
+            //li.style = "color:red";
+        }
+        let childrenUl;
+
+        if (isChoosen && dec != null) {
+            const str = key;
+            const match = str.match(/^(\w+)<=(\w+)$/);
+
+            if (match != null) {
+                const header = match[1];
+                const value = match[2];
+
+                console.log(dec);
+                console.log(value);
+                console.log(header);
+                console.log(dec[header])
+
+                if (dec[header] <= value) {
+                    childrenUl = createTreeDom(obj[key], dec, "Left", isChoosen = true);
+                    //li.classList.add("userDecison");
+                }
+                else {
+                    childrenUl = createTreeDom(obj[key], dec, "Right", isChoosen = true);
+                    //li.classList.add("red");
+                }
+            }
+            else childrenUl = createTreeDom(obj[key], dec, "", isChoosen = true);
+        }
+        else childrenUl = createTreeDom(obj[key], dec, "", isChoosen = false);
+
+        if (childrenUl) {
+            li.append(childrenUl);
+        }
+
+        ul.append(li);
+
+    }
+    isChoosen = false;
+    return ul;
+}
+
+
+function build_tree(data) {
+
+    let maxGain = -1;
+    let bestSplit = [];
+
+    let question;
+    let headers = data[0];
+    headers = Object.getOwnPropertyNames(headers);
+
+
+    if (data.length == 1) return { [`predict = ${data[0][headers[headers.length - 1]]}`]: {} };
+
+
+    console.log(data);
+
+    for (let h = 0; h < headers.length - 1; h++) {
+        for (let i = 0; i < data.length; i++) {
+
+            //console.log(headers[h]);
+
+            let value = data[i][headers[h]];
+
+            // console.log(value);
+
+            let left = []
+            let right = []
+            let all_data = []
+
+            for (let j = 0; j < data.length; j++) {
+                if (data[j][headers[h]] <= value) {
+                    left.push([j, data[j][headers[headers.length - 1]]])
+                }
+                else right.push([j, data[j][headers[headers.length - 1]]])
+                all_data.push([j, data[j][headers[headers.length - 1]]])
+            }
+
+            if (left.length == 0 || right.length == 0) continue
+
+            gain = informationGain(all_data, left, right)
+            if (gain == -1) {
+                return { [`predict = ${all_data[0][1]}`]: {} }
+
+                /*
+                return {
+                    [`${headers[h]}<=${value}?`]: {
+                        "YES": {
+                            [`predict = ${left[0][1]}`]: {}
+                        }
+                        ,
+                        "NO": {
+                            [`predict = ${right[0][1]}`]: {}
+                        }
+                    }
+                }
+                */
+            }
+
+            if (gain > maxGain) {
+
+                maxGain = gain;
+                bestSplit = [left, right]
+                question = `${headers[h]}<=${value}`;
+            }
+
+        }
+    }
+
+
+
+    best_left = [];
+    best_right = [];
+
+    for (let j = 0; j < bestSplit[0].length; j++) {
+        best_left.push(data[bestSplit[0][j][0]])
+    }
+    for (let j = 0; j < bestSplit[1].length; j++) {
+        best_right.push(data[bestSplit[1][j][0]])
+    }
+
+
+    console.log(best_left, question)
+
+    return {
+        [question]: {
+            "YES": build_tree(best_left),
+            "NO": build_tree(best_right)
+        }
+    }
+
+    /*
+    return {
+        [question]:
+            Object.assign(
+                {},
+                build_tree(best_left),
+                build_tree(best_right)
+            )
+    }
+    */
+}
+
+// let data =
+//     build_tree(
+//         [
+//             { 'age': '50', 'ap_hi': '110', 'cardio': '0' },
+//             { 'age': '55', 'ap_hi': '140', 'cardio': '1' },
+//             { 'age': '52', 'ap_hi': '130', 'cardio': '1' },
+//             { 'age': '48', 'ap_hi': '150', 'cardio': '1' },
+//             { 'age': '48', 'ap_hi': '100', 'cardio': '0' },
+//             { 'age': '60', 'ap_hi': '150', 'cardio': '0' }
+//         ])
+
+
 let button = document.querySelector('.buttonOfSave');
 let table = document.querySelector('table');
 table.hidden = true;
 
+let buttonChose = document.querySelector('.buttonOfSaveChoose');
+buttonChose.style.display = "none";
 
 
+let textAreaInput = document.querySelector('.textAreaChoose');
+textAreaInput.style.display = "none";
 
 let titles = document.querySelectorAll('.title');
 
-for(let title = 0; title < titles.length; title++) {
+for (let title = 0; title < titles.length; title++) {
     titles[title].hidden = true;
 }
 
+let data;
 
 button.onclick = function () {
 
 
     //появление заголовков по нажатию
-    for(let title = 0; title < titles.length; title++) {
+    for (let title = 0; title < titles.length; title++) {
         titles[title].hidden = false;
     }
-    
+
+    buttonChose.style.display = "block";
 
     let text = document.querySelector('.textArea').value;
     let textArea = '';
 
-
+    textAreaInput.style.display = "block";
 
     //убрать пробелы из строки //
     for (let i = 0; i < text.length; i++) {
@@ -61,8 +276,18 @@ button.onclick = function () {
 
     //data - готовый объект в формате csv //
 
-    let data = dataArray;
-    //console.log(data);
+    data = dataArray;
+    // console.log(data);
+
+
+
+    //рисует дерево
+    let tree = document.querySelector('.tree');
+    createTree(tree, build_tree(data));
+    // console.log(build_tree(data));
+
+
+
 
 
 
@@ -122,21 +347,21 @@ button.onclick = function () {
     let k = 0; //идёт по всему массиву tableData
 
 
-    for(let i = 0; i < heightOfTable; i++) {
+    for (let i = 0; i < heightOfTable; i++) {
 
         let tr = document.createElement('tr'); //создаём tr
 
-        for(let j = 0; j < headers.length; j++) {
+        for (let j = 0; j < headers.length; j++) {
 
             let td = document.createElement('td'); //создаём tr
             td.textContent = tableData[k]; //добавляем значение в td
             tr.appendChild(td);
             k++;
         }
-        
+
         table.appendChild(tr); //заносим в table готовую таблицу
     }
-    
+
     //добавление в div таблицы
     div.appendChild(table);
 
@@ -148,57 +373,105 @@ button.onclick = function () {
 
 
 
+    // let data_exs = {
+    //     "Рыбы": {
+    //         "форель": {},
+    //         "лосось": {}
+    //     },
+
+    //     "Деревья": {
+    //         "Огромные": {
+    //             "секвойя": {},
+    //             "дуб": {}
+    //         },
+    //         "Цветковые": {
+    //             "яблоня": {},
+    //             "магнолия": {}
+    //         }
+    //     }
+    // }
+
+    // function createTree(container, data) {
+    //     container.append(createTreeDom(data));
+    // }
+
+    // function createTreeDom(obj) {
+    //     if (!Object.keys(obj).length) return;
+
+    //     let ul = document.createElement('ul');
+
+    //     for(let key in obj) {
+    //         let li = document.createElement('li');
+    //         li.innerHTML = key;
+
+    //         let childrenUl = createTreeDom(obj[key]);
+    //         if (childrenUl) {
+    //             li.append(childrenUl);
+    //         }
+
+    //         ul.append(li);
+    //     }
+
+    //     return ul;
+    // }
+
+    // let treeList = document.querySelector('.treeList');
+    // createTree(treeList, data_exs);
 
 
-    let data_exs = {
-        "Рыбы": {
-            "форель": {},
-            "лосось": {}
-        },
-    
-        "Деревья": {
-            "Огромные": {
-                "секвойя": {},
-                "дуб": {}
-            },
-            "Цветковые": {
-                "яблоня": {},
-                "магнолия": {}
-            }
-        }
-    }
-    
-    function createTree(container, data) {
-        container.append(createTreeDom(data));
-    }
-    
-    function createTreeDom(obj) {
-        if (!Object.keys(obj).length) return;
-    
-        let ul = document.createElement('ul');
-    
-        for(let key in obj) {
-            let li = document.createElement('li');
-            li.innerHTML = key;
-    
-            let childrenUl = createTreeDom(obj[key]);
-            if (childrenUl) {
-                li.append(childrenUl);
-            }
-    
-            ul.append(li);
-        }
-    
-        return ul;
-    }
-    
-    let treeList = document.querySelector('.treeList');
-    createTree(treeList, data_exs);
-    
+
 }
 
+let dataDec = 0;
+
+buttonChose.onclick = function () {
+    let inputDataDec = document.querySelector('.textAreaChoose').value.split(',');
+    //let inputDataDecArea = ''
+    // console.log(inputDataDeg);
 
 
+    /*
+    //убрать пробелы из строки //
+    for (let i = 0; i < inputDataDec.length; i++) {
+        if (inputDataDec[i] != ' ') {
+            inputDataDecArea += inputDataDec[i];
+        }
+    }
+    */
+
+    let obj = {};
+    let headers = data[0];
+    headers = Object.getOwnPropertyNames(headers);
+
+    for (let i = 0; i < inputDataDec.length; i++) {
+        obj[headers[i]] = inputDataDec[i];
+    }
+    
+    console.log(obj);
+    /*
+    const csvData = inputDataDecArea;
+
+    // Parse the CSV data using Papa Parse library
+    const parsedData = Papa.parse(csvData, { header: true }).data;
+
+    // Create an array of objects from the parsed CSV data
+    const dataArray = parsedData.map(row => {
+        const obj = {};
+        Object.keys(row).forEach(key => {
+            obj[key] = row[key];
+            });
+        return obj;
+    });
+
+    */
+    
+
+    let tree = document.querySelector('.tree');
+
+    document.querySelector('.tree').innerHTML = "";
+
+    createTree(tree, build_tree(data), obj);
+}
 
 
 
